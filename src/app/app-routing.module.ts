3ab6c40f83ed68055481ba10d6989e1ec16001b1
Xel4ek <l-baal@yandex.ru>
2020-12-22 13:38:20 +0300

    auth form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+// import {LoginComponent} from "@components/login/login.component";
+// import {RegisterComponent} from "@components/register/register.component";
+// import {RestoreComponent} from "@components/restore/restore.component";
+import {AuthLayoutComponent} from "@components/auth-layout/auth-layout.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: AuthLayoutComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+
+import { DataService } from '@services/data.service';
+import { PluginsService } from '@services/plugins.service';
+
 import { AppComponent } from './app.component';
 
+import { HeaderComponent } from "@components/header/header.component";
+import { FooterComponent } from "@components/footer/footer.component";
+
+import { AuthLayoutModule } from "@components/auth-layout/auth-layout.module";
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, HeaderComponent, FooterComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AuthLayoutModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [DataService, PluginsService],
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
