0157e01144d04fc73389b60fe9b791a884d7758d
Xel4ek <l-baal@yandex.ru>
2020-12-23 16:47:06 +0300

    finish auth part

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+import {WebsocketService} from "@services/websocket/websocket.service";
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
 
-  constructor(public router: Router) {
-
+  constructor(public router: Router,
+              private wsService: WebsocketService,
+  ) {
   }
+
+  ngOnInit(): void {
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,8 @@ import { ToastrModule } from 'ngx-toastr';
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 import { HttpClientModule }   from '@angular/common/http';
-
 import { AppRoutingModule } from './app-routing.module';
-
+import { WebsocketModule } from '@services/websocket/websocket.module';
 import { HttpService } from '@services/http.service';
 import { PluginsService } from '@services/plugins.service';
 
@@ -16,7 +15,8 @@ import { HeaderComponent } from "@components/header/header.component";
 import { FooterComponent } from "@components/footer/footer.component";
 
 import { AuthLayoutModule } from "@components/auth-layout/auth-layout.module";
-import { SettingsComponent } from './components/settings/settings.component';
+import { SettingsComponent } from '@components/settings/settings.component';
+import {environment} from "../environments/environment";
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent, FooterComponent, SettingsComponent],
@@ -27,6 +27,9 @@ import { SettingsComponent } from './components/settings/settings.component';
     HttpClientModule,
     BrowserAnimationsModule,
     ToastrModule.forRoot(),
+    WebsocketModule.config({
+      url: environment.ws
+    })
   ],
   providers: [HttpService, PluginsService],
   bootstrap: [AppComponent],
