2e8da667d329e9db8276dfab7708e62700574576
Xel4ek <l-baal@yandex.ru>
2021-03-07 17:07:38 +0300

    some more

--- src/app/services/user/user.ts
+++ src/app/services/user/user.ts
@@ -7,6 +7,7 @@ export class User extends UserInfo{
   blackList: string[] =  [];
   activeChats: string[] = [];
   email: string = '';
+  matches: string[] = [];
   constructor() {
     super();
   }
