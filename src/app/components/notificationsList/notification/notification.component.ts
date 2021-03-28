209468f91219c80f89781ddd86b8fc418ae47417
Xel4ek <l-baal@yandex.ru>
2021-03-28 20:47:33 +0300

    fix notification for mobile

--- src/app/components/notificationsList/notification/notification.component.ts
+++ src/app/components/notificationsList/notification/notification.component.ts
@@ -1,15 +1,21 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterViewInit, Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { NotificationMessage } from "@components/notificationsList/notification.interface";
 import { NotificationService } from "@services/notification/notification.service";
+import { DeviceDetectorService } from "@services/device-detector/device-detector.service";
+import { Subject } from "rxjs";
+import { takeUntil } from "rxjs/operators";
 
 @Component({
   selector: 'app-notification[entry]',
   templateUrl: './notification.component.html',
   styleUrls: ['./notification.component.scss']
 })
-export class NotificationComponent implements OnInit {
+export class NotificationComponent implements OnInit, OnDestroy, AfterViewInit {
   @Input() entry!: NotificationMessage;
-  constructor(private ns: NotificationService) {
+  private destroy$ = new Subject<void>();
+  constructor(private ns: NotificationService,
+              private deviceDetector: DeviceDetectorService) {
+
   }
   remove() {
     this.ns.edit({id: this.entry.id, action: 'remove'});
@@ -20,6 +26,18 @@ export class NotificationComponent implements OnInit {
     }
   }
   ngOnInit(): void {
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete()
+  }
+
+  ngAfterViewInit(): void {
+    this.deviceDetector.isMobile$.pipe(takeUntil((this.destroy$))).subscribe(isMobile => {
+      if (isMobile)
+        this.checked();
+    })
   }
 
 }
